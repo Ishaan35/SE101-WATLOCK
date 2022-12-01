82a92f1150c78d771f117f0f7962989f7b2ce5bb
Jason Milad <jmilad@uwaterloo.ca>
2022-12-01 15:33:35 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,38 +1,108 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Image} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function App() {
+
+const Stack = createNativeStackNavigator();
+
+
+function Form() {
+
+  const [email, setEmail] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+async function sendData() {
+  let url = 'https://bike-lock-server.onrender.com/login';
+  let data= {email: email, password: pwd};
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache', 
+    headers: {
+      Accept: "application/json",
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  });
+  const response_ = await response.json();
+  console.log(response_);
+
+ if(response_ && response_.email){
+    setLoggedIn(true);
+    setEmail(response_.email);
+    setPwd(response_.password);
+    navigation.navigate('Home');
+}
+}
+
+
+return(<View style={styles.container}>
+  <Text> WATLOCK for students</Text>
+
+  <Image
+        style={styles.image}
+        source={{
+          uri: 'https://uwaterloo.ca/brand/sites/ca.brand/files/styles/body-500px-wide/public/uploads/images/university-of-waterloo-vertical-logo.png?itok=9KCQdLsy',
+        }}
+      />
+  <TextInput type = "email" style = {styles.input} required placeholder="someone@example.com"  value={email} onChangeText={(e)=>setEmail(e)}/>
+  <TextInput secureTextEntry={true} style = {styles.input} required placeholder="password" onChangeText={(e)=>
+    setPwd(e) }/>
+
+ <Button  title="Login" color = '#9c64a6' style = {styles.input} onPress = {sendData}/>
+</View>);
+
+
+}
+
+function Home() {
+
   return (
-    <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
-      />
-      <text> hello </text>
-    </SafeAreaView>
+    <View style = {styles.container}>
+ <Text>Im cool</Text>
+    </View>
   );
-};
 
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <NavigationContainer>
+    <Stack.Navigator>
+
+<Stack.Screen name="Form" component={Form}/>
+<Stack.Screen name="Home" component={Home}  title = {`Welcome, {email}`}
+/>
+      {/* <StatusBar style="auto" /> */}
+      </Stack.Navigator>
+
+    </NavigationContainer>
+    </View>
+
+  );
+}
+  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  }, input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },  
+    marginTop: 50,
+    marginBottom: 50,
+  }, input:{
+    height: 50,
+    margin: 10,
+    borderWidth:'thick',
+    borderRadius:30,
+    padding: 15,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
 });
-
-export default App  ;
